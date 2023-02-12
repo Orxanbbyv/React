e3a14dd01bada3c795307f27d2d52529c7287def
Orxanbbyv <116581123+Orxanbbyv@users.noreply.github.com>
2023-02-12 23:35:38 +0400

    Proje

---
+++ Tours/src/App.jsx
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from "react";
+import Loading from "./Loading";
+import Tours from "./Tours";
+const url = "https://course-api.com/react-tours-project";
+function App() {
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour) => tour.id !== id);
+    setTours(newTours);
+  };
+
+  const featchTours = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(url);
+      const tours = await response.json();
+      setLoading(false);
+      setTours(tours);
+    } catch (error) {
+      setLoading(true);
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    featchTours();
+  }, []);
+
+  if (loading) {
+    return <Loading />;
+  }
+  if (tours.length === 0) {
+    return (
+      <main>
+        <div className="title">
+          <h2> no tours left</h2>
+          <button className="btn" onClick={featchTours}>
+            refresh
+          </button>
+        </div>
+      </main>
+    );
+  }
+  return <Tours tours={tours} removeTour={removeTour} />;
+}
+export default App;
